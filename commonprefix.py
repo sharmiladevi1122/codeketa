f03d989924df2c479f752db4547be1c1cfc0370f
sharmiladevi1122 <51734632+sharmiladevi1122@users.noreply.github.com>
2019-07-03 15:58:43 +0530

    Update commonprefix.py

--- commonprefix.py
+++ commonprefix.py
@@ -1,10 +1,18 @@
 #sharmila
 n=int(input())
 in1=[]
+in2=[]
 for i in range(0,n):
   in1.append(input())
-for i in range(0,len(min(in1))):
-  if in1[0][i]==in1[1][i]:
+for i in in1:
+  in2.append(len(i))
+j1=0
+for i in range(0,min(in2)):
+  for j in range(1,n):
+    if in1[0][i]==in1[j][i]:
+      j1=0
+    else:
+      j1=1
+      break
+  if j1==0:
     print(in1[0][i],end='')
-  else:
-    break
